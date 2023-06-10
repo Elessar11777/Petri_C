f016baaa03b3f144da61d208aeb652e827310871
Elessar11777 <Elessar11777@gmail.com>
2023-06-10 12:36:44 +0300

    07.06.2023v added logger

---
+++ logger.py
@@ -0,0 +1,33 @@
+import logging
+from datetime import datetime
+import os
+def setup_logger():
+    # Create a custom logger
+    logger = logging.getLogger(__name__)
+
+    # Set the level to INFO. This means that logger will handle all messages of level INFO and above
+    logger.setLevel(logging.DEBUG)
+
+    # Create handlers
+    c_handler = logging.StreamHandler()
+
+    today = datetime.today().strftime('%d_%m_%Y')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    f_handler = logging.FileHandler(f"./logs/logs_{today}.log")
+    c_handler.setLevel(logging.INFO)
+    f_handler.setLevel(logging.DEBUG)
+
+    # Create formatters and add it to handlers
+    format = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(format)
+    f_handler.setFormatter(format)
+
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+
+    return logger
+
+
+aeya_logger = setup_logger()
